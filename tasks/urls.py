910a2fe4438883b4229b73e7b10a5ec89aaf5c2e
pratham kumar jha <prathamjha123dcs@gmail.com>
2024-12-09 20:43:32 +0530

    Revert "aage mamala bigadne waala hai"
    This reverts commit 509c304c6a644329cd724724f5fac04d2ebf301a.

--- tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "tasks"
-
-urlpatterns = [
-    path("addNotification/", views.CreateTaskView.as_view(), name="create notifiaction"),
-    path("getalltasks/", views.GetTaskView.as_view(), name="get task")
-]
